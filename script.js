3e7ba5cf30f9568fd89486c7b10d3b033c238630
Sakethbalakumar <balakumar.2@osu.edu>
2025-02-17 14:26:11 -0500

    Day 2

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ Tasks 1 - 5 prioritize basic DOM manipulation
 */
 //Task 1: When page loads, change <h1> text to "Welcome to JavaScript Practice"
 document.addEventListener('DOMContentLoaded', function(){
-document.getElementById('header').innerText = "Welcome to JavaScript Practice!";
+    document.getElementById('header').innerText = "Welcome to JavaScript Practice!!!!";
+
 });
 
 //Task 2: When clicking run javascript test button, change the <p> text to "ButtonClicked"
@@ -21,7 +22,7 @@ document.getElementById('header').innerText = "Welcome to JavaScript Practice!";
 
 document.getElementById('runTest').addEventListener('click', paragraphFunc);
 function paragraphFunc(){
-    document.getElementById('output').textContent = "Button Clicked";
+    document.getElementById('output').textContent = "Button Clicked, list added and removed, console logged";
     let list = document.getElementById('itemList');
     list.append(document.createElement('li'));
     let remover = list.getElementsByTagName('li');
@@ -30,23 +31,43 @@ function paragraphFunc(){
 
 //Task 3: When user types in input field and clicks "Submit", display the input in a new p tag - <p id = "displayText"></p>
 function processInput(){
-    let content = document.getElementById('userInput').value;
-    document.getElementById('displayText').textContent = content;
+    let submitContent = document.getElementById('userInput').value;
+    document.getElementById('userInput').textContent = submitContent;
 }
 
 /*
-    Tasks 6-9 Prioritize more advanced DOM manipulation and functions such as timers, counting multiple clicks, etc.
-*/
-//Task 6: Add a new button called counter
-//Task 7: Count how many <li> tags there are
-//Task 8: Count how many times the counter button is clicked and increment every time
-//Task 9: Update <p> every 10 seconds with current time
-
-/*
-    These next set of tasks are designed to delve deeper in scope and states
+    These next set of tasks are designed to delve deeper into closures, synch vs asynch code, API, local storage
 */
 
-//Task 10: Count how many times "run javacript test" button is clicked with a closure
-//Task 11: Store "submit" user input in local storage
-//Task 12: fetch data from a public API
-//Task 13: Display in a list
+//Task 6: Count how many times "run javacript test" button is clicked with a closure
+//Task 7: Modify runTest so that it logs "start" to the console and then after 5 seconds logs "end" (asynch code)
+//Task 8: Promise
+//Task 9: Fetch a random joke from an API (https://official-joke-api.appspot.com/random_joke) and display it in a new <p> when the button is clicked
+
+
+//Task 6: Closure (scope of a function in another function)
+//Create function to return a variable count. Declare a const counter and then add an event listener to the button
+function addCounter(){
+    let count = 0;
+    return function(){
+        count++;
+        return count;
+    };
+}
+
+const counter = addCounter();
+
+document.getElementById('runTest').addEventListener('click', function(){
+    console.log(counter());
+});
+
+//Task 7: Asynch code to log "start" and then "end" after 5 seconds
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('start');
+    setTimeout(function(){
+        console.log('end');
+    },5000);
+});
+
+//Task 8: Promise async / await
